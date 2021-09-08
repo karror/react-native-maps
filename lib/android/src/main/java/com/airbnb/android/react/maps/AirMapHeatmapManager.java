0c8ea762b3e60cfc841bae0ea56f353936d66df9
karol <sliwkakarol@gmail.com>
2021-09-08 10:48:29 +0200

    heatmap files replace

--- lib/android/src/main/java/com/airbnb/android/react/maps/AirMapHeatmapManager.java
+++ lib/android/src/main/java/com/airbnb/android/react/maps/AirMapHeatmapManager.java
@@ -2,73 +2,58 @@ package com.airbnb.android.react.maps;
 
 import com.facebook.react.bridge.ReadableArray;
 import com.facebook.react.bridge.ReadableMap;
-import com.facebook.react.uimanager.annotations.ReactProp;
 import com.facebook.react.uimanager.ThemedReactContext;
 import com.facebook.react.uimanager.ViewGroupManager;
-
-import com.google.android.gms.maps.model.LatLng;
-import com.google.maps.android.heatmaps.WeightedLatLng;
-import com.google.maps.android.heatmaps.Gradient;
-
+import com.facebook.react.uimanager.annotations.ReactProp;
 
 public class AirMapHeatmapManager extends ViewGroupManager<AirMapHeatmap> {
-
     @Override
     public String getName() {
         return "AIRMapHeatmap";
     }
 
     @Override
-    public AirMapHeatmap createViewInstance(ThemedReactContext context) {
-        return new AirMapHeatmap(context);
+    protected AirMapHeatmap createViewInstance(ThemedReactContext reactContext) {
+        return new AirMapHeatmap(reactContext);
     }
 
     @ReactProp(name = "points")
     public void setPoints(AirMapHeatmap view, ReadableArray points) {
-        WeightedLatLng[] p = new WeightedLatLng[points.size()];
-        for (int i = 0; i < points.size(); i++) {
-            ReadableMap point = points.getMap(i);
-            WeightedLatLng weightedLatLng;
-            LatLng latLng = new LatLng(point.getDouble("latitude"), point.getDouble("longitude"));
-            if (point.hasKey("weight")) {
-                weightedLatLng = new WeightedLatLng(latLng, point.getDouble("weight"));
-            } else {
-                weightedLatLng = new WeightedLatLng(latLng);
-            }
-            p[i] = weightedLatLng;
-        }
-        view.setPoints(p);
+        view.setPoints(points);
+    }
+
+    @ReactProp(name = "radius", defaultInt = 10)
+    public void setRadius(AirMapHeatmap view, int radius) {
+        view.setRadius(radius);
     }
 
     @ReactProp(name = "gradient")
     public void setGradient(AirMapHeatmap view, ReadableMap gradient) {
-        ReadableArray srcColors = gradient.getArray("colors");
-        int[] colors = new int[srcColors.size()];
-        for (int i = 0; i < srcColors.size(); i++) {
-            colors[i] = srcColors.getInt(i);
-        }
-
-        ReadableArray srcStartPoints = gradient.getArray("startPoints");
-        float[] startPoints = new float[srcStartPoints.size()];
-        for (int i = 0; i < srcStartPoints.size(); i++) {
-            startPoints[i] = (float)srcStartPoints.getDouble(i);
-        }
-
-        if (gradient.hasKey("colorMapSize")) {
-            int colorMapSize = gradient.getInt("colorMapSize");
-            view.setGradient(new Gradient(colors, startPoints, colorMapSize));
-        } else {
-            view.setGradient(new Gradient(colors, startPoints));
-        }
+        view.setGradient(gradient);
     }
 
-    @ReactProp(name = "opacity")
+    @ReactProp(name = "opacity", defaultDouble = 0.7)
     public void setOpacity(AirMapHeatmap view, double opacity) {
         view.setOpacity(opacity);
     }
 
-    @ReactProp(name = "radius")
-    public void setRadius(AirMapHeatmap view, int radius) {
-        view.setRadius(radius);
+    @ReactProp(name = "maxIntensity", defaultDouble = 0)
+    public void setMaxIntensity(AirMapHeatmap view, double maxIntensity) {
+        view.setMaxIntensity(maxIntensity);
+    }
+
+    @ReactProp(name = "gradientSmoothing", defaultDouble = 10)
+    public void setGradientSmoothing(AirMapHeatmap view, double gradientSmoothing) {
+        view.setGradientSmoothing(gradientSmoothing);
+    }
+
+    @ReactProp(name = "heatmapMode")
+    public void setHeatmapMode(AirMapHeatmap view, String heatmapMode) {
+        view.setHeatmapMode(heatmapMode);
+    }
+
+    @ReactProp(name = "onZoomRadiusChange")
+    public void onZoomRadiusChange(AirMapHeatmap view, ReadableMap onZoomRadiusChange) {
+        view.setOnZoomRadiusChange(onZoomRadiusChange);
     }
 }
